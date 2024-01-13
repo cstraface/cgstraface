faaf722f2d8528aef7ada8076f2c77cdf0f59aeb
CS <cstrafa@gmail.com>
2024-01-13 09:47:00 -0700

    trying the capt

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import PreLoader from "@/src/layout/PreLoader";
 import "@/styles/globals.css";
 import State from "../src/context/context";
 
+<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+</script>
+
 export default function App({ Component, pageProps }) {
   return (
     <State>
