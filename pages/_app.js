adaddc4c6fcbdc9521dd7c3dce65abe6f77bcec1
CS <cstrafa@gmail.com>
2024-01-19 11:23:02 -0700

    adding GA tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import PreLoader from "@/src/layout/PreLoader";
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "@/styles/globals.css";
 import State from "../src/context/context";
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights/>
+      <GoogleAnalytics gaId="G-V5V4TW2X8W" />
     </State>
   );
 }
