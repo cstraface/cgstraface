b291773da507e6b8bc73439b1d2b2897501d507d
CS <cstrafa@gmail.com>
2024-01-20 12:46:06 -0700

    add robots

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   env:{
     SITE_KEY: process.env.SITE_KEY
+  },
+  async rewrites() {
+    return [
+        {
+            source: '/robots.txt',
+            destination: '/api/route'
+        }
+    ];
   }
 }
-
 module.exports = nextConfig
