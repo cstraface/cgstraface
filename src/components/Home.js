927258b2704763486fc6f5fa5830210e71f999e6
CS <cstrafa@gmail.com>
2024-01-22 16:30:32 -0700

    update homepage skills carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { context } from "../context/context";
 import SectionContainer from "./SectionContainer";
 const homeData = {
-  skills: ["Guru", "Admin", "Manager"],
+  skills: ["Expert", "Guru", "Admin", "Manager"],
 };
 const Home = () => {
   const [text, setText] = useState(0);
